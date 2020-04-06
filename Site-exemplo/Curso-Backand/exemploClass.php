8bbd421a97ebdb1effd76241024d6cd7f8de6996
Josielcosta138 <cjosiel2@gmail.com>
2020-04-05 22:56:07 -0300

    Create exemploClass.php
    Criado exemplo Classe em PHP

---
+++ Site-exemplo/Curso-Backand/exemploClass.php
@@ -0,0 +1,44 @@
+<?php
+
+	/**
+	 * 	Está classe é de EXEMPLO!
+	 */
+	class Exemplo
+
+	{
+			private $var1;
+			public $var2;
+
+			/*
+			public static $var3 = "Statico";
+
+			public function metodo (){
+ 
+			}
+
+			public static function metodoStatico(){
+				//echo "Metodo Static";
+
+
+			}
+
+			private function metodo2 (){
+
+			}
+				*/
+			
+		
+
+			public function setVar1($var1){
+				$this->var1 = $var1;
+				
+			}
+
+			public function pegaVar1(){
+				return $this->var1;
+			}
+	}
+
+
+
+?>
